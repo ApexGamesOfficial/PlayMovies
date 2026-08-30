7144ff12fd6380234e6969f592d8a22792673ef6
Apex Games <jfranke1230@gmail.com>
2026-08-29 23:20:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const searchButton = document.getElementById("searchButton");
+const searchPanel = document.getElementById("searchPanel");
+const closeSearch = document.getElementById("closeSearch");
+const searchInput = document.getElementById("searchInput");
 
+searchButton.addEventListener("click", () => {
+
+    searchPanel.classList.add("open");
+
+    setTimeout(() => {
+        searchInput.focus();
+    }, 100);
+
+});
+
+closeSearch.addEventListener("click", () => {
+
+    searchPanel.classList.remove("open");
+
+});
+
+document.addEventListener("keydown", (event) => {
+
+    if (event.key === "Escape") {
+        searchPanel.classList.remove("open");
+    }
+
+});
